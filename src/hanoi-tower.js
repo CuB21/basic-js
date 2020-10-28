d855e0187a1317dc4646303fde4ea62614fd9b5d
unknown <igor.yakubovsky@gmail.com>
2020-10-28 15:51:26 +0300

    check connection

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,10 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi( disksNumber, turnsSpeed ) {
-  let resalt = {};
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  let result = {};
   let turns = Math.pow(2, disksNumber) -1;
-  let time = Math.floor((turns / turnsSpeed) * 3600)
-  resalt.turns = turns;
-  resalt.seconds = time;
-  return resalt;
+  let time = Math.floor((turns / turnsSpeed) * 3600);
+  result.turns = turns;
+  result.seconds = time;
+  return result;
 };
